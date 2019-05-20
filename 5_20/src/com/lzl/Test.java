ae25850260a50587e4e3a2d5dea88d237403bdd5
qigemingzizhememafan <43529959+qigemingzizhememafan@users.noreply.github.com>
2019-05-20 19:00:28 +0800

    数组基础

---
+++ 5_20/src/com/lzl/Test.java
@@ -0,0 +1,117 @@
+package com.lzl;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Arrays;
+//public class Test{
+//    public static void main(String[] args) {
+//        int[] arr1 = new int[]{1,3,5,7,9};
+//        int[] arr2 = new int[]{2,4,6,8,10};
+//        System.arraycopy(arr1,1,arr2,1,3);
+//        int[] data = Arrays.copyOfRange(arr1,1,7);
+//        printArr(data);
+//    }
+//        int[] data = new int[]{1,5,3,2,90,76};
+//        char[] c = new char[]{'c','b','g','l'};
+//        Arrays.sort(data); //对数组元素排序
+//        Arrays.sort(c);
+//        printArr(data);
+//        printArr(c);
+//    }
+//    public static void printArr(int[] data){
+//        for(int x:data){
+//            System.out.print(x+"、");
+//        }
+//    }
+//    public static void printArr(char[] c){
+//        for(char temp:c){
+//            System.out.print(temp+"、");
+//        }
+//    }
+//public class Test {
+//        public static void printArr(int[] data){
+//            for(int x:data){
+//                System.out.print(x+"、");
+//            }
+//        }
+//        public static void printArr(char[] c){
+//            for(char temp:c){
+//                System.out.print(temp+"、");
+//            }
+//        }
+//
+//    }
+//    public static int[] init(){
+//        return new int[]{1,2,3,4,5};
+//    }
+//    public static void printArr(int[] array){
+//        for(int i = 0; i < array.length; i++){
+//            System.out.println(array[i]);
+//        }
+//    }
+//    public static void modify(int[] array){
+//        for(int i = 0; i < array.length; i++){
+//            array[i] = array[i]+100;
+//            System.out.println(array[i]);
+//        }
+//    }
+//    public static void main(String[] args) {
+//            init();
+//            printArr(init());
+//            modify(init());
+//        int[][] data = new int[][]{{1,2,3},{4,5,6},{7,8,9,0}};
+//        for(int i = 0; i < data.length;i++){
+//            for(int j = 0; j < data[i].length; j++){
+//                System.out.print(data[i][j]);
+//            }
+//            System.out.println();
+//        }
+//         System.out.println(new int[]{1,4,3}[0]);
+//         int[] data = null;
+//        data[0] = 1;
+//        for(int s:data){
+//            System.out.println(s);
+//        }
+//        String[] data = new String[10];
+//        for(int i = 0; i < 10; i++){
+//            data[i] = "abc"+i+"、";
+//        }
+//        for(String x:data){
+//            System.out.print(x);
+//        }
+//        switchx(2);
+//        switchx(4);
+//        Scanner scanner = new Scanner(System.in);
+//        String s = scanner.next();
+//        switch (s){
+//            case "abc":
+//                System.out.println("abc");
+//            case "def":
+//                System.out.println("def");
+//            default:
+//                System.out.println("hehehe");
+//        }
+
+//        System.out.println("请输入一个字符");
+//        char c = (char)System.in.read();
+//        switch (c){
+//            case 'a':
+//                System.out.println("this is a");
+//                break;
+//            case 'b':
+//                System.out.println("this is b");
+//                break;
+//            default:
+//                System.out.println("hahahaha");
+//                break;
+//    }
+//        public static void switchx(int a){
+//            if (a == 2) {
+//                return;
+//            }else {
+//                System.out.println(a);
+//            }
+//        }
+//    }
+//}
+
